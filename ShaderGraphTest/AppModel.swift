6fbb80776d539504877c490456ba80fad3d0ac3a
John Brewer <jbrewer@jera.com>
2025-08-13 19:11:16 -0700

    Initial Commit

---
+++ ShaderGraphTest/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  ShaderGraphTest
+//
+//  Created by John Brewer on 8/13/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ ShaderGraphTest/ShaderGraphTestApp.swift
@@ -0,0 +1,34 @@
+//
+//  ShaderGraphTestApp.swift
+//  ShaderGraphTest
+//
+//  Created by John Brewer on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ShaderGraphTestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
